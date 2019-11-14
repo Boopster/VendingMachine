0252b94fce59b49242f57451b28849aa600a9d29
Rebecca Still <beckystill@hotmail.com>
2019-11-14 19:41:53 +0000

    Create a Product abstract class which has name and brand

---
+++ src/main/java/Product.java
@@ -0,0 +1,13 @@
+//Create a Product abstract class which has name and brand.
+
+public abstract class Product {
+
+    private String name;
+    private String brand;
+
+    public Product(String name, String brand) {
+        this.name = name;
+        this.brand = brand;
+    }
+
+}
