9848fb3e9c54c1ac612f7f303ade5f2633687195
Rebecca Still <beckystill@hotmail.com>
2019-11-14 20:54:25 +0000

    Create a CoinReturn class with a list of coins

---
+++ src/main/java/machine/CoinReturn.java
@@ -0,0 +1,15 @@
+package machine;
+
+import coins.Coin;
+
+import java.util.ArrayList;
+
+public class CoinReturn {
+
+    private ArrayList<Coin> coins;
+
+    public CoinReturn() {
+        this.coins = new ArrayList<Coin>();
+    }
+
+}
--- src/test/java/DrawerTest.java
+++ src/test/java/DrawerTest.java
@@ -1,8 +1,9 @@
 import machine.Drawer;
 import org.junit.Before;
 import org.junit.Test;
-import products.Product;
 import products.Sweet;
+import products.Crisp;
+import products.Drink;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -10,11 +11,15 @@ public class DrawerTest {
 
     Drawer drawer;
     Sweet sweet;
+    Crisp crisp;
+    Drink drink;
 
     @Before
     public void before() {
         drawer = new Drawer();
         sweet = new Sweet("Mars Bar","Mars");
+        crisp = new Crisp("Salt and Vinegar", "Walkers");
+        drink = new Drink("Fanta", "The Coca-Cola Co")
     }
 
     @Test
