5357f8c366b54ed6160b80e1ccc6a902f5b11348
Rebecca Still <beckystill@hotmail.com>
2019-11-14 19:37:39 +0000

    add Coin class and getters for enums

---
+++ src/main/java/Coin.java
@@ -0,0 +1,17 @@
+public class Coin {
+
+    private CoinType coin;
+
+    public Coin(CoinType coin) {
+        this.coin = coin;
+    }
+
+    public CoinType getCoin() {
+        return this.coin;
+    }
+
+    public int getValueFromEnum() {
+        return this.coin.getValue();
+    }
+
+}
