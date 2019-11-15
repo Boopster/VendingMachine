5c705fbf65f6564e22d781de7801bd0d7680c315
Rebecca Still <beckystill@hotmail.com>
2019-11-15 09:12:32 +0000

    create a Vending Machine class with a list of drawers, list of coins entered and coin return

---
+++ src/main/java/machine/VendingMachine.java
@@ -0,0 +1,21 @@
+//Create a Vending Machine class with a list of drawers, list of coins entered and coin return.
+
+package machine;
+
+import coins.Coin;
+
+import java.util.ArrayList;
+
+public class VendingMachine {
+
+    private ArrayList<Drawer> drawers;
+    private ArrayList<Coin> coins;
+    private CoinReturn coinReturn;
+
+    public VendingMachine() {
+        this.drawers = new ArrayList<Drawer>();
+        this.coins = new ArrayList<Coin>();
+        this.coinReturn = new CoinReturn();
+    }
+
+}
